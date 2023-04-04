020ca47461f52e12612a1fa7f9f7e18af62662e7
DreamBoyT <115417933+DreamBoyT@users.noreply.github.com>
2023-04-04 19:02:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+/*FOR BUTTON CLICKS*/
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
+    sound(this.innerHTML);
+    anime(this.innerHTML);
+  })
+}
+
+/*FOR KEY PRESS*/
+document.addEventListener("keydown", function(event) {
+  sound(event.key);
+  anime(event.key);
+})
+
+function sound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    default:
+      console.log(this.innerHTML)
+  }
+}
+
+function anime(currentKey) {
+  var str = document.querySelector("." + currentKey);
+  str.classList.add("pressed");
+  setTimeout(function() {
+    str.classList.remove("pressed");
+  }, 100);
+
+}
